1d16d141d9bb22f5913177cb714b19e3e798f215
SivaaDurai <sivasankari0864@gmail.com>
2024-07-21 16:39:25 +0530

    Task Completed

--- src/org/git/GitHub.java
+++ src/org/git/GitHub.java
@@ -3,6 +3,7 @@ package org.git;
 public class GitHub {
  public static void main(String[] args) {
 	 System.out.println("Practing the Github using");
+	 System.out.println("practing the day 2 class");
 	
 }
 }
